4f8ce005ad89ec0415dc8d8ddeac1fb1beb6c0d2
lebleuciel <93124243+lebleuciel@users.noreply.github.com>
2023-10-01 18:41:10 +0330

    add comments

--- solution2/trie.js
+++ solution2/trie.js
@@ -2,10 +2,13 @@ const characterLimit = 27
 
 function getCharCode(char) {
   if (char === "'") {
+    // Return a unique code for single quotes
     return 26
   } else if (char >= 'a' && char <= 'z') {
+    // Return the character code for lowercase letters
     return char.charCodeAt(0) - 'a'.charCodeAt(0)
   } else {
+    // Return -1 for other characters (not in the range a-z, or ')
     return -1
   }
 }
@@ -18,13 +21,15 @@ function getCodeChar(code) {
   }
 }
 
+// Define the Node structure for the Trie
 class TrieNode {
   constructor() {
-    this.count = 0
-    this.parent = null
-    this.children = Array(characterLimit).fill(null)
+    this.count = 0 // Number of times this word is present
+    this.parent = null // Reference to parent node
+    this.children = Array(characterLimit).fill(null) // Array to hold child nodes for each letter
   }
 
+  // Create and set a child node for a given character if it doesn't exist
   createChild(char) {
     const charCode = getCharCode(char)
     if (!this.children[charCode]) {
@@ -35,11 +40,13 @@ class TrieNode {
   }
 }
 
+// Define the Trie structure
 class Trie {
   constructor() {
     this.root = new TrieNode()
   }
 
+  // Add a string to the Trie
   addString(str) {
     let currentNode = this.root
     for (const char of str) {
