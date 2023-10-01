4926adfeabd8d4da23b60ad02d80b1eb75b46187
lebleuciel <93124243+lebleuciel@users.noreply.github.com>
2023-10-01 13:24:37 +0330

    add comments

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
+// This function takes an input string and returns an array of the top three most frequently used words in it.
 function top_3_words(inputString) {
-    const lowerCaseString = inputString.toLowerCase();
-    const cleanedString = lowerCaseString.replace(/[^a-z']+/g, ' ');
-    const words = cleanedString.split(' ');
-    const wordFrequency = {};
-
-    words.forEach(word => {
-        if (word !== '') {
-            if (wordFrequency[word]) {
-                wordFrequency[word]++;
-            } else {
-                wordFrequency[word] = 1;
-            }
-        }
-    });
-
-    const sortedWords = Object.keys(wordFrequency).sort((a, b) => wordFrequency[b] - wordFrequency[a]);
-    return sortedWords.slice(0, 3);
+  // Convert the input string to lowercase to ensure case-insensitive word counting.
+  const lowerCaseString = inputString.toLowerCase()
+  // Replace all characters that are not letters or apostrophes with spaces to isolate words.
+  const cleanedString = lowerCaseString.replace(/[^a-z']+/g, ' ')
+  // Split the cleaned string into individual words and store them in an array.
+  const words = cleanedString.split(' ')
+  const wordFrequency = {}
+  // Count the frequency of each word.
+  words.forEach((word) => {
+    // Ensure that empty strings (resulting from consecutive spaces) are not counted
+    if (word !== '') {
+      if (wordFrequency[word]) {
+        wordFrequency[word]++
+      } else {
+        wordFrequency[word] = 1
+      }
+    }
+  })
+  // Sort the words by their frequencies in descending order.
+  const sortedWords = Object.keys(wordFrequency).sort(
+    (a, b) => wordFrequency[b] - wordFrequency[a],
+  )
+  // Return the top three words with the highest frequencies.
+  return sortedWords.slice(0, 3)
 }
-
